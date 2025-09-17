71eef11593c49874b196e344da001d5a43bf5214
EslamKamel89 <elsamkamelforex@gmail.com>
2025-09-17 07:23:56 +0300

    feat(db): set up SQLAlchemy ORM base
    - Configure SQLite engine with thread safety
    - Initialize sessionmaker and declarative Base
    - Remove placeholder FastAPI route

--- database.py
+++ database.py
@@ -1,6 +1,9 @@
-from fastapi import FastAPI
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-app = FastAPI()
-@app.get('/')
-async def home():
-    return {"message":"welcome to FastTask app"}
+SQLALCHEMY_DATABASE_URL = r"sqlite:///.\fast_task.db" 
+engine = create_engine(SQLALCHEMY_DATABASE_URL , connect_args={'check_same_thread':False})
+
+SessionLocal = sessionmaker(autocommit=False , autoflush=False , bind=engine)
+Base = declarative_base()
