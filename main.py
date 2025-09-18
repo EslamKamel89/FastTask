53f82ecf831b59c685e8c6bc752da2eaca00325f
EslamKamel89 <elsamkamelforex@gmail.com>
2025-09-18 13:37:39 +0300

    feat(api): add todo listing endpoint - Init DB models and dependency - Expose /todos to fetch all records

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
-from fastapi import FastAPI
+from typing import Annotated
 
-import models
+from fastapi import Depends, FastAPI
+from sqlalchemy.orm import Session
+
 from database import Base, SessionLocal, engine  # type: ignore
+from models import Todo
 
-models.Base.metadata.create_all(bind=engine)
+Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally :
+        db.close()
+
 
 app = FastAPI()
+db_dependency = Annotated[Session , Depends(get_db)]
+
 @app.get('/')
 async def home() :
     return {"message": "hello from FastAPI"}
+
+@app.get('/todos') 
+async def all_todos(db:db_dependency) :
+    return db.query(Todo).all()
