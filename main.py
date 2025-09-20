198cfefeff75c1a53d8084e26f7da97162a8b957
EslamKamel89 <elsamkamelforex@gmail.com>
2025-09-20 10:07:08 +0300

    feat(api): add todo CRUD endpoints and schemas - Add GET /todos/{id} with validation and 404 handling - Define Pydantic models for todo read/create

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from typing import Annotated
 
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, HTTPException, Path, status
 from sqlalchemy.orm import Session
 
 from database import Base, SessionLocal, engine  # type: ignore
-from models import Todo
+from models import Todo, TodoRead  # type: ignore
 
 Base.metadata.create_all(bind=engine)
 
@@ -23,6 +23,13 @@ db_dependency = Annotated[Session , Depends(get_db)]
 async def home() :
     return {"message": "hello from FastAPI"}
 
-@app.get('/todos') 
+@app.get('/todos' , status_code=status.HTTP_200_OK) 
 async def all_todos(db:db_dependency) :
     return db.query(Todo).all()
+
+@app.get('/todos/{todo_id}' , status_code=status.HTTP_200_OK , response_model=TodoRead)
+async def get_todo(db:db_dependency , todo_id:Annotated[int , Path(ge=1 , description='todo id >= 1')]) : 
+    todo = db.query(Todo).filter(Todo.id==todo_id).first()
+    if todo is None :
+        raise HTTPException(status_code=404 , detail=f"No todo with id: {todo_id} exist")
+    return todo
--- models.py
+++ models.py
@@ -1,4 +1,7 @@
-from sqlalchemy import Boolean, Column, Integer, String
+from typing import Annotated  # type: ignore
+
+from pydantic import BaseModel, ConfigDict, Field  # type: ignore
+from sqlalchemy import Boolean, Column, Integer, String  # type: ignore
 
 from database import Base
 
@@ -9,4 +12,14 @@ class Todo(Base) :
     title = Column(String(255))
     description = Column(String(255) , nullable=True)
     priority = Column(Integer)
-    complete = Column(Boolean, default=False)
+    
+class TodoBase(BaseModel):
+    title:str 
+    description:str |None = None
+    priority:int 
+    complete:bool = False 
+class TodoRead(TodoBase) :
+    id :int 
+    model_config = ConfigDict(from_attributes=True)
+class TodoCreate(TodoBase) :
+    pass
