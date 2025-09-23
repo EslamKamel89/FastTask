f04a18cd9732f530b5609fc789c29543de32b65d
EslamKamel89 <elsamkamelforex@gmail.com>
2025-09-23 11:28:21 +0300

    feat(models): add User ORM and schemas - Define User table with auth fields - Add Pydantic schemas: Base, Read, Create - Link Todo to User via owner_id

--- models.py
+++ models.py
@@ -1,11 +1,38 @@
 from typing import Annotated  # type: ignore
 
 from pydantic import BaseModel, ConfigDict, Field  # type: ignore
-from sqlalchemy import Boolean, Column, Integer, String  # type: ignore
+from sqlalchemy import (Boolean, Column, ForeignKey, Integer,  # type: ignore
+                        String)
 
 from database import Base
 
 
+class User(Base) :
+    __tablename__ = 'users'
+    id = Column(Integer , primary_key=True , index=True)
+    username = Column(String ,unique=True, index=True)
+    email = Column(String , unique=True , index=True)
+    hashed_password = Column(String)
+    first_name = Column(String)
+    last_name = Column(String)
+    is_active = Column(Boolean , default=True)
+    role = Column(String)
+
+class UserBase(BaseModel) :
+    username:str = Field(min_length=3 , max_length=255)
+    email:str = Field(min_length=3 , max_length=255)
+    password:str = Field(min_length=3 , max_length=255)
+    first_name:str = Field(min_length=3 , max_length=255)
+    last_name:str = Field(min_length=3 , max_length=255)
+    is_active:bool = False
+    role:str = Field(min_length=3 , max_length=255)
+
+class UserRead(UserBase) :
+    id : int
+    model_config = ConfigDict(from_attributes=True)
+
+class UserCreate(UserBase) :
+    pass
 class Todo(Base) :
     __tablename__ = 'todos'
     id = Column(Integer , primary_key=True , index=True)
@@ -13,7 +40,7 @@ class Todo(Base) :
     description = Column(String(255) , nullable=True)
     priority = Column(Integer)
     complete = Column(Boolean , default=False)
-    
+    owner_id = Column(Integer, ForeignKey('users.id'))
 class TodoBase(BaseModel):
     title:str = Field(min_length=3 , max_length=255)
     description:str |None = Field(None, min_length=3 , max_length=255 )
