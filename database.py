0a3d47a2ea2ce2147bcb496dd598f26511091974
EslamKamel89 <elsamkamelforex@gmail.com>
2025-09-30 22:14:08 +0300

    fix(db): correct connect_args for non-SQLite databases - Replace None with empty dict for PostgreSQL

--- database.py
+++ database.py
@@ -7,7 +7,7 @@ SQLITE:str = r"sqlite:///.\fast_task.db"
 POSTGRESQL:str=r"postgresql://postgres:password@localhost/todo"
 SELECTED_DB : str = POSTGRESQL
 
-engine = create_engine(SELECTED_DB , connect_args={'check_same_thread':False} if SELECTED_DB == SQLITE else None ) # type: ignore
+engine = create_engine(SELECTED_DB , connect_args={'check_same_thread':False} if SELECTED_DB == SQLITE else {} ) # type: ignore
 
 SessionLocal = sessionmaker(autocommit=False , autoflush=False , bind=engine)
 Base = declarative_base()
