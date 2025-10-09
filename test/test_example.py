7605f4144a591c83a45c53a874678f6c919c90d0
EslamKamel89 <elsamkamelforex@gmail.com>
2025-10-09 07:09:36 +0300

    test: add Student class and fixture-based tests - Introduce Student model and pytest fixture - Add manual and fixture-driven initialization tests

--- test/test_example.py
+++ test/test_example.py
@@ -1,3 +1,6 @@
+import pytest
+
+
 def test_equal_or_not_equal():
     assert 3 == 3
     assert 3 != 1
@@ -26,3 +29,28 @@ def test_list():
     assert 7 not in num_list
     assert all(num_list)
     assert not any(any_list)
+    
+    
+class Student:
+    def __init__(self , first_name:str , last_name:str , major:str , years:int):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.major = major
+        self.years = years
+
+@pytest.fixture
+def default_employee():
+    return  Student('Eslam' , 'Kamel' , 'Computer Science' , 5)
+        
+def test_person_initialization(): # manual instantiation
+    p = Student('Eslam' , 'Kamel' , 'Computer Science' , 5)
+    assert p.first_name == 'Eslam' , 'first name should be Eslam'
+    assert p.last_name == 'Kamel' , 'last name should be Kamel'
+    assert p.major == 'Computer Science'
+    assert p.years == 5
+    
+def test_person_initialization2(default_employee:Student): # automatic instantiation
+    assert default_employee.first_name == 'Eslam' , 'first name should be Eslam'
+    assert default_employee.last_name == 'Kamel' , 'last name should be Kamel'
+    assert default_employee.major == 'Computer Science'
+    assert default_employee.years == 5
