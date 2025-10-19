990865b5e6a62fdafec679aca49b7858d9eebe89
EslamKamel89 <elsamkamelforex@gmail.com>
2025-10-19 13:26:15 +0300

    feat(test): add JWT access token creation test - Verify token generation and payload integrity for authenticated users

--- test/test_auth.py
+++ test/test_auth.py
@@ -1,10 +1,13 @@
+from datetime import timedelta
 from test.utils import *
 from typing import Generator
 
 from fastapi import status
+from jose import jwt
 
 from models import Todo, User  # type: ignore
 from routers.auth import authenticate_user
+from security import ALGORITHM, SECRET_KEY, create_access_token
 
 
 def test_authenticate_user(test_user: Generator[User]):
@@ -15,4 +18,10 @@ def test_authenticate_user(test_user: Generator[User]):
     user = authenticate_user('eslam' , 'wrong' , db)
     assert user == False
     
-
+def test_create_access_token(test_user:Generator[User]) : 
+    token = create_access_token(test_user.username , test_user.id , test_user.role , timedelta(days=1) ) # type: ignore
+    assert token != '' 
+    payload = jwt.decode(token , SECRET_KEY , algorithms=[ALGORITHM])
+    assert test_user.username == payload.get('sub') # type: ignore
+    assert test_user.id == payload.get('id') # type: ignore
+    assert test_user.role == payload.get('role') # type: ignore
