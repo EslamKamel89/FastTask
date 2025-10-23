8006f1f6d8aac580081a072cec51c315fe1aa238
EslamKamel89 <elsamkamelforex@gmail.com>
2025-10-23 14:46:35 +0300

    feat(api): add home page with Jinja2 templating - Render home.html using FastAPI and Jinja2Templates

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 
 from database import Base, engine
 from routers import admin, auth, todos, users
 
 app = FastAPI()
 Base.metadata.create_all(bind=engine)
+
+templates = Jinja2Templates('template')
+
 @app.get('/healthy')
 async def health_check():
     return {"status":"Healthy"}
+
+@app.get('/')
+async def home(request:Request) :
+    return templates.TemplateResponse("home.html" , {
+        "request" : request,
+    })
 
 app.include_router(auth.router)
 app.include_router(todos.router)
